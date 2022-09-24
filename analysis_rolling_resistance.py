f38f76c3bc96dc9bc9b169e87670b8f0a8115593
TheRealAndrewSpain <spainandrew@yahoo.com>
2022-09-24 16:01:17 -0500

    These were apparently deleted

---
+++ analysis_rolling_resistance.py
@@ -0,0 +1,30 @@
+from subfunctions import *
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def main():
+    
+    #Initialize Variables
+    terrain_angle = 0
+    omega = 2.0
+    Crr_array = np.linspace(0.01, 0.4, num=25)
+    forces = np.zeros(len(Crr_array))
+
+    #Calculate Net Forces
+    for i in range(len(Crr_array)):
+        forces[i] = F_net(omega, terrain_angle, MARVIN_DICT['rover'], MARVIN_DICT['planet'], Crr_array[i])
+
+    #Calculate Net Accelerations
+    accelerations = forces / get_mass(MARVIN_DICT['rover'])
+
+    #Plot acceleration vs. Crr
+    plt.plot(Crr_array, accelerations)
+    plt.title('Acceleration vs. Rolling Resistance Coefficient')
+    plt.ylabel('Acceleration (m/s^2)')
+    plt.xlabel('Crr')
+    plt.show()
+
+
+if __name__ == '__main__':
+        main()
