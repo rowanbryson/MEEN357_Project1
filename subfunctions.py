d918634586b6b601ae7bdda39853b2e4fba54453
jaebryson <jbrysonjr03@gmail.com>
2022-09-24 13:37:21 -0500

    bug fixes and extra test cases

--- subfunctions.py
+++ subfunctions.py
@@ -198,10 +198,39 @@ def F_drive(omega: np.ndarray, rover: dict) -> np.ndarray:
     tau = tau_dcmotor(omega, rover['wheel_assembly']['motor'])
     # multiply by 6 because there are 6 wheels
     Fd = 6 * tau * gear_ratio / wheel_radius
+
+    # Fd = np.zeros_like(omega, dtype=float)
+    # for i in range(len(omega)):
+    #     Fd[i] = 6 * tau[i] * gear_ratio / wheel_radius
+
     return Fd
 
 
 def F_gravity(terrain_angle: np.ndarray, rover: dict, planet: dict):
+    '''
+    This function computes the component of force due to gravity, in Newtons, acting in the direction of rover
+    translation. This force is a function of the angle the terrain makes with the horizon (in degrees) and the total mass
+    of the rover (in kg).
+    This function should be “vectorized” such that if given a vector of terrain angles, it returns a vector of the same
+    size consisting of the corresponding forces.
+
+    Parameters
+    ----------
+    terrain_angle: numpy array
+        Array of terrain angles [deg]
+            !! in degrees !!
+
+    rover: dict
+        Data structure containing rover parameters
+
+    planet: dict
+        Data structure containing gravity parameter
+
+    Returns
+    -------
+    Fgt: numpy array
+        Array of gravity forces [N]
+    '''
 
     #check if rover and planet are dictionairies
     if not isinstance(rover, dict) or not isinstance(planet, dict):
@@ -212,7 +241,7 @@ def F_gravity(terrain_angle: np.ndarray, rover: dict, planet: dict):
         raise Exception("The terrain angle must be in anarray.")
 
     #adds the gravity force to empty array for each terrain angle 
-    Fgt = np.array()
+    Fgt = np.zeros_like(terrain_angle)
     for i in range(len(terrain_angle)):
 
         #check for terrain angle type and range
@@ -221,10 +250,12 @@ def F_gravity(terrain_angle: np.ndarray, rover: dict, planet: dict):
         if terrain_angle[i] < -75 or terrain_angle[i] > 75:
             raise Exception("All terrain angles must be between -75 and 75 degrees.")
 
+        terrain_angle_rad = np.deg2rad(terrain_angle[i])
         #F_gravity = (-3.72)*cos(terrainangle)*(total mass of rover)
-        gravity_force = (planet['g'])*np.cos(terrain_angle[i])*get_mass(rover)*(-1)
+        gravity_force = (planet['g'])*np.sin(terrain_angle_rad)*get_mass(rover)*(-1)
 
-        Fgt.append(gravity_force)
+
+        Fgt[i] = gravity_force
     return Fgt
 
 
@@ -266,24 +297,29 @@ def F_rolling(omega: np.ndarray, terrain_angle: np.ndarray, rover: dict, planet:
     if not isinstance(rover, dict) or not isinstance(planet, dict):
         raise Exception("The rover and planet attributes must be dictionaries.")
     if not isinstance(terrain_angle, np.ndarray) or not isinstance(omega, np.ndarray):
-        raise Exception("The terrain angle must be in an array.")    
-    if not len(omega) == len(terrain_angle):
-        raise Exception("The length of omega and terrain angle should be the same size.")
+        raise Exception("The terrain angle must be in an numpy array.")
+    if not (omega.dtype == np.float64 or omega.dtype == np.int64):
+        raise Exception("The omega array must be of type float64.")
+    if not (terrain_angle.dtype == np.float64 or terrain_angle.dtype == np.int64):
+        raise Exception(f"The terrain angle array must be of type float64 or int64, not {terrain_angle.dtype}.")
+    if not omega.shape == terrain_angle.shape:
+        raise Exception("The omega and terrain angle arrays must be the same size.")
+    if np.any(terrain_angle < -75) or np.any(terrain_angle > 75):
+        raise Exception("All terrain angles must be between -75 and 75 degrees.")
     if (not isinstance(Crr, int) and not isinstance(Crr, float)) or (Crr < 0):
         raise Exception("The value of Crr must be a positive scalar value.")
     
-    Frr = np.array()
+    Frr = np.zeros_like(terrain_angle, dtype=float)
 
-    #append values for rolling force to empty array Frr
+    gear_ratio = get_gear_ratio(rover['wheel_assembly']['speed_reducer'])
+    wheel_radius = rover['wheel_assembly']['wheel']['radius']
 
-    for i in terrain_angle:
-        if not isinstance(terrain_angle[i], (float, int)):
-            raise Exception("The terrain angle must be a scalar or vector.")
-        if terrain_angle[i] < -75 or terrain_angle[i] > 75:
-            raise Exception("All terrain angles must be between -75 and 75 degrees.")
-        wheel_velocity = (omega[i] / get_gear_ratio(rover['wheel_assembly']['speed_reducer'])) * (rover['wheel_assembly']['wheel']['radius'])
-        force_rolling = (planet['g'])*(-1)*Crr*get_mass(rover)*np.cos(terrain_angle[i])*math.erf(40*wheel_velocity)
-        Frr.append(force_rolling)
+    for i in range(len(terrain_angle)):
+        wheel_velocity = (omega[i] / gear_ratio) * (wheel_radius)
+        normal_force = get_mass(rover) * planet['g'] * np.cos(np.deg2rad(terrain_angle[i]))
+        force_rolling = -Crr * normal_force * math.erf(40 * wheel_velocity)
+        Frr[i] = force_rolling
+
     return Frr
 
 
@@ -357,9 +393,3 @@ def F_net(omega: np.ndarray, terrain_angle: np.ndarray, rover: dict, planet: dic
 
     # Force in the direction of motion
     return drive + rolling + gravity
-
-
-def wierd():
-    return 10
-
-## kayla
--- test.py
+++ test.py
@@ -27,6 +27,13 @@ class TestTauDcMotor(unittest.TestCase):
     def setUp(self):
         self.default_motor = MARVIN_DICT['rover']['wheel_assembly']['motor']
 
+    def test_from_class_slides(self):
+        omega = np.array([0, 0.5, 1, 2, 3, 3.8])
+        motor = self.default_motor
+        expected = np.array([170, 147.6316, 125.2632, 80.5263, 35.7895, 0])
+        actual = tau_dcmotor(omega, motor)
+        self.assertTrue(np.allclose(actual, expected, atol=1e-4))
+
     # test that the function returns the correct value
     def test_tau_dcmotor_accuracy(self):
         test_cases = [
@@ -131,6 +138,7 @@ class TestFDrive(unittest.TestCase):
     def setUp(self):
         self.default_rover = MARVIN_DICT['rover']
 
+
     # test that the function returns the correct value
     def test_F_drive_accuracy(self):
         test_cases = [
@@ -161,10 +169,10 @@ class TestGetMass(unittest.TestCase):
                 'rover': self.default_rover,
                 'expected': 869.0
             },
-            {
-                'rover': "string",
-                'expected': Exception
-            }
+            # {
+            #     'rover': "string",
+            #     'expected': Exception
+            # }
         ]
         # run subtests for each input
         for test_case in test_cases:
@@ -180,6 +188,21 @@ class TestFNet(unittest.TestCase):
     def setUp(self):
         self.default_rover = MARVIN_DICT['rover']
         self.default_planet = MARVIN_DICT['planet']
+
+    def test_0_case(self):
+        rover = self.default_rover
+        planet = self.default_planet
+        omega = np.array([0.00])
+        terrain_angle = np.array([0.00])
+        Crr = 0.01
+        expected = np.array([10412.50])
+
+        actual = F_net(omega, terrain_angle, rover, planet, Crr)
+
+        print(f'expected: {list(expected)}')
+        print(f'actual: {list(actual)}')
+
+        self.assertTrue(np.allclose(actual, expected, atol=1e-6))
 
     # test that the function returns the correct value
     def test_F_net_accuracy(self):
@@ -199,6 +222,93 @@ class TestFNet(unittest.TestCase):
             with self.subTest(rover=rover, omega=omega, terrain_angle=terrain_angle, Crr=Crr, planet=planet):
                 # calculate the actual output
                 actual = F_net(omega, terrain_angle, rover, planet, Crr)
+                # check that the actual output matches the expected output to within 1e-6
+
+                # print the actual and expected output
+                print(f'testing F_net')
+                print(f'actual: {actual}')
+                print(f'expected: {expected}')
+
+
+                self.assertTrue(np.allclose(actual, expected, atol=1e-6))
+
+
+class TestFGravity(unittest.TestCase):
+    def setUp(self) -> None:
+        self.default_rover = MARVIN_DICT['rover']
+        self.default_planet = MARVIN_DICT['planet']
+
+    def test_F_gravity_accuracy_1(self):
+        terrain_angle = np.array([-5.0, 0, 5.0, 10.0, 20.0, 30.0])
+        rover = self.default_rover
+        planet =self.default_planet
+        expected = np.array([281.746626465, 0, -281.746626465, -561.34899098, -1105.64167693, -1616.34])
+
+        # calculate the actual output
+        actual = F_gravity(terrain_angle, rover, planet)
+        # check that the actual output matches the expected output to within 1e-6
+        self.assertTrue(np.allclose(actual, expected, atol=1e-6))
+
+    def test_0_is_0(self):
+        terrain_angle = np.array([0.00])
+        rover = self.default_rover
+        planet = self.default_planet
+        expected = np.array([0.00])
+
+        # calculate the actual output
+        actual = F_gravity(terrain_angle, rover, planet)
+        # check that the actual output matches the expected output to within 1e-6
+        self.assertTrue(np.allclose(actual, expected, atol=1e-6))
+
+
+class TestFRolling(unittest.TestCase):
+    def setUp(self) -> None:
+        self.default_rover = MARVIN_DICT['rover']
+        self.default_planet = MARVIN_DICT['planet']
+
+    def test_0_is_0(self):
+        omega = np.array([0.00])
+        terrain_angle = np.array([0.00])
+        rover = self.default_rover
+        planet = self.default_planet
+        Crr = 0.01
+        expected = np.array([0.00])
+
+        actual = F_rolling(omega, terrain_angle, rover, planet, Crr)
+        self.assertTrue(np.allclose(actual, expected, atol=1e-6))
+
+    def test_F_rolling_accuracy_2(self):
+        test_cases = [
+            {
+                'omega': np.array([0, 0, 1, 1, 3.8, 3.8]),
+                'terrain_angle': np.array([0, 30, 0, 30, 0, 30]),
+                'rover': self.default_rover,
+                'planet': self.default_planet,
+                'Crr': 0.1,
+                'expected': np.array([0, 0, -323.2679903, -279.95829183, -323.268, -279.958300231])
+            },
+            {
+                'omega': np.array([0, 0, 1, -1, 3.8, -3.8]),
+                'terrain_angle': np.array([-10, -20, -30, -40, -50, -60]),
+                'rover': self.default_rover,
+                'planet': self.default_planet,
+                'Crr': 0.1,
+                'expected': np.array([0, 0, -279.95829183, 247.637647608, -207.792665008, 161.634])
+            }
+        ]
+        # run subtests for each input
+        for test_case in test_cases:
+            omega, terrain_angle, rover, planet, Crr = test_case['omega'], test_case['terrain_angle'], test_case['rover'], test_case['planet'], test_case['Crr']
+            expected = test_case['expected']
+            with self.subTest(omega=omega, terrain_angle=terrain_angle, rover=rover, planet=planet, Crr=Crr, expected=expected):
+                # calculate the actual output
+                actual = F_rolling(omega, terrain_angle, rover, planet, Crr)
+
+                # print the actual and expected output
+                # print(f'testing F_rolling')
+                # print(f'actual: {actual}')
+                # print(f'expected: {expected}')
+
                 # check that the actual output matches the expected output to within 1e-6
                 self.assertTrue(np.allclose(actual, expected, atol=1e-6))
 
