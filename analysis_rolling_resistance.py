efd175d11b21b89f2ead70875eeb3f7d1b1195c9
TheRealAndrewSpain <spainandrew@yahoo.com>
2022-09-26 13:33:31 -0500

    Finished the functionality of analysis_rolling_resistance.py, may or may not be correct, but it is functional.

--- analysis_rolling_resistance.py
+++ analysis_rolling_resistance.py
@@ -1,30 +1,31 @@
 from subfunctions import *
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.optimize import root_scalar
+from scipy.optimize import root
 
-
-def main(save_plots=False):
+def main():
     
     #Initialize Variables
     terrain_angle = 0
-    omega = 2.0
     Crr_array = np.linspace(0.01, 0.4, num=25)
-    forces = np.zeros(len(Crr_array))
+    v_max = np.zeros(len(Crr_array), dtype=float)
+    omega_nl = MARVIN_DICT['rover']['wheel_assembly']['motor']['speed_noload']
+    wheel_radius = MARVIN_DICT['rover']['wheel_assembly']['wheel']['radius']
+    rover = MARVIN_DICT['rover']
+    planet = MARVIN_DICT['planet']
 
-    #Calculate Net Forces
-    for i in range(len(Crr_array)):
-        forces[i] = F_net(omega, terrain_angle, MARVIN_DICT['rover'], MARVIN_DICT['planet'], Crr_array[i])
-
-    #Calculate Net Accelerations
-    accelerations = forces / get_mass(MARVIN_DICT['rover'])
+    for ii in range(len(Crr_array)):
+        fun = lambda omega: F_net(np.array([omega]), np.array([float(terrain_angle)]), rover, planet, Crr_array[ii])
+        sol = root_scalar(fun, method='bisect', bracket=[0, omega_nl])
+        omega_max = sol.root
+        v_max[ii] = wheel_radius * 6 * omega_max
 
     #Plot acceleration vs. Crr
-    plt.plot(Crr_array, accelerations)
-    plt.title('Acceleration vs. Rolling Resistance Coefficient')
-    plt.ylabel('Acceleration (m/s^2)')
+    plt.plot(Crr_array, v_max)
+    plt.title('Max Velocity vs. Rolling Resistance Coefficient')
+    plt.ylabel('V Max (m/s)')
     plt.xlabel('Crr')
-    if save_plots:
-        plt.savefig('plots/analysis_rolling_resistance.png')
     plt.show()
 
 
