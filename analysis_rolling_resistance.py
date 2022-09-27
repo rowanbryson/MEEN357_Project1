2aee7f5f1496eb7e9e41584b6feb59bd2c8f5d80
TheRealAndrewSpain <spainandrew@yahoo.com>
2022-09-27 08:37:06 -0500

    Fixed a last minute error in the analysis_terrain and analysis_rolling calculations to accurately reflect the lab slides. Updated the plots and the final zip file documentation.

--- analysis_rolling_resistance.py
+++ analysis_rolling_resistance.py
@@ -4,7 +4,7 @@ import numpy as np
 from scipy.optimize import root_scalar
 from scipy.optimize import root
 
-def main(save_plots=False):
+def main(save_plots=True):
     
     #Initialize Variables
     terrain_angle = 0
@@ -14,12 +14,13 @@ def main(save_plots=False):
     wheel_radius = MARVIN_DICT['rover']['wheel_assembly']['wheel']['radius']
     rover = MARVIN_DICT['rover']
     planet = MARVIN_DICT['planet']
+    gear_ratio = get_gear_ratio(rover['wheel_assembly']['speed_reducer'])
 
     for ii in range(len(Crr_array)):
         fun = lambda omega: F_net(np.array([omega]), np.array([float(terrain_angle)]), rover, planet, Crr_array[ii])
         sol = root_scalar(fun, method='bisect', bracket=[0, omega_nl])
         omega_max = sol.root
-        v_max[ii] = wheel_radius * 6 * omega_max
+        v_max[ii] = omega_max / gear_ratio * rover['wheel_assembly']['wheel']['radius']
 
     #Plot acceleration vs. Crr
     plt.plot(Crr_array, v_max)
@@ -27,7 +28,7 @@ def main(save_plots=False):
     plt.ylabel('V Max (m/s)')
     plt.xlabel('Crr')
     if save_plots:
-        plt.savefig('plots/analysis_terrain_slope.png')
+        plt.savefig('plots/analysis_rolling_resistance.png')
     plt.show()
 
 
--- analysis_terrain_slope.py
+++ analysis_terrain_slope.py
@@ -4,7 +4,7 @@ from scipy.optimize import root_scalar
 from scipy.optimize import root
 import numpy as np
 
-def main(save_plots=False):
+def main(save_plots=True):
     Crr = .2
     slope_list_deg = np.linspace(-10, 35, 25)
     v_max = np.zeros(len(slope_list_deg), dtype=float)
@@ -12,12 +12,13 @@ def main(save_plots=False):
     wheel_radius = MARVIN_DICT['rover']['wheel_assembly']['wheel']['radius']
     rover = MARVIN_DICT['rover']
     planet = MARVIN_DICT['planet']
+    gear_ratio = get_gear_ratio(rover['wheel_assembly']['speed_reducer'])
 
     for ii in range(len(slope_list_deg)):
         fun = lambda omega: F_net(np.array([omega]), np.array([float(slope_list_deg[ii])]), rover, planet, Crr)
         sol = root_scalar(fun, method='bisect', bracket=[0, omega_nl])
         omega_max = sol.root
-        v_max[ii] = wheel_radius * 6 * omega_max
+        v_max[ii] = omega_max / gear_ratio * rover['wheel_assembly']['wheel']['radius']
 
     plt.plot(slope_list_deg, v_max)
     plt.title("Slope [deg] vs Maximum Velocity [m/s]")
